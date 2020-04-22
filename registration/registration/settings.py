151a00cf06220bbb2055ace39c930b8b2557f64f
shaikhsdf <sdfworkk@gmail.com>
2020-04-22 19:14:46 +0530

    Update settings.py

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = secret.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['demoudaan.herokuapp.com',
+'http://shaikhsdf.pythonanywhere.com/',
+'127.0.0.1',
+
+]
 
 
 # Application definition
